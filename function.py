8f16a8d8c58bf31c79ed110f824c3240135d3399
adityajain256 <166713822+adityajain256@users.noreply.github.com>
2025-03-20 08:48:34 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,26 @@
+#q1 function for adding number
+
+# def sum(a,b):
+#         return a+b
+
+# print(sum(5,5))
+
+# q2 for calculating factorial using recursion
+
+# def factorial(n):
+#         m = 1
+#         for i in range(1, n+1):
+#              m = m*i 
+             
+#         return m 
+
+# print(factorial(4)) 
+
+# def factorial(n):
+#         if n == 1 or n == 0:
+#                 return 1
+
+#         else:
+#                 return n*factorial(n-1)
+        
+# print(factorial(4))
